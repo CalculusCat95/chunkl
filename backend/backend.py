ded8495b72ba8035d678bf5e71761d9e99968077
CalculusCat95 <david.hokuf@gmail.com>
2022-04-11 10:28:39 +0300

    cleaned up code in 'backend' and added .gitignore
    Co-authored-by: C0LateralD4mage <C0LateralD4mage@users.noreply.github.com>

--- backend/backend.py
+++ backend/backend.py
@@ -1,2 +1,11 @@
+""" 
+This file contains the 'build' function, which is exposed in javascript in the GUI.
+It is executed when the user initiates a build in the GUI. It then opens the project file path
+and begins the appropriate build toolchain."
+
+"""
+
+## Placeholder function; will be implemented soon.
 def build(platform, path):
+    
     print("Build Initiated! \n Platform: ", platform, "\n Path: ", path)
